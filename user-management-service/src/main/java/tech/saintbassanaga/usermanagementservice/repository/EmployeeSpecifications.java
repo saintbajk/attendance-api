48506c60f84471923cd782db8dfaacaaa2a23cf4
saintbassanaga <saintbassanaga01@gmail.com>
2024-07-15 06:07:26 +0100

    feat: Adding Employee finding Specification that find all employees in one salary interval

---
+++ user-management-service/src/main/java/tech/saintbassanaga/usermanagementservice/repository/EmployeeSpecifications.java
@@ -0,0 +1,27 @@
+package tech.saintbassanaga.usermanagementservice.repository;
+
+import jakarta.persistence.criteria.*;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.NonNull;
+import tech.saintbassanaga.usermanagementservice.entities.Employee;
+
+import java.math.BigDecimal;
+
+public class EmployeeSpecifications {
+    public static Specification<Employee> salaryBetween(BigDecimal minSalary, BigDecimal maxSalary){
+        return new Specification<Employee>() {
+            @Override
+            public Predicate toPredicate(@NonNull Root<Employee> root, @NonNull CriteriaQuery<?> query, @NonNull CriteriaBuilder criteriaBuilder) {
+                if (minSalary != null && maxSalary != null) {
+                    return criteriaBuilder.between(root.get("position").get("salary"), minSalary, maxSalary);
+                } else if (minSalary != null) {
+                    return criteriaBuilder.greaterThanOrEqualTo(root.get("salary"), minSalary);
+                } else if (maxSalary != null) {
+                    return criteriaBuilder.lessThanOrEqualTo(root.get("salary"), maxSalary);
+                } else {
+                    return criteriaBuilder.conjunction();
+                }
+            }
+        };
+    }
+}
